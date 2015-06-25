b837394ea27b34f978aa217b1b722116eef6e046
AL08F13 <dianafca123@gmail.com>
2015-06-25 12:18:02 -0500

    uno
    las carpetas

---
+++ js/acciones.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(document).ready(function(e) {
+document.addEventListener("deviceready",function(){
+	$('#mypanel').on('tap',function (){
+		navigator.vibrate(1000);
+	});
+	
+	
+}); 
+});
+
